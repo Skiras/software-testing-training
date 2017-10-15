71f7fd2bc533b8fcce44cb2e764565569f534c92
abodykin <abodykin@inttrust.ru>
2017-10-15 13:39:13 +0300

    Задание 7. Сделайте сценарий, проходящий по всем разделам админки

---
+++ javaexample/src/test/java/ru/stqa/training/selenium/Task7.java
@@ -0,0 +1,36 @@
+package ru.stqa.training.selenium;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * лекция: 4. Поиск элементов
+ * задание: Задание 7. Сделайте сценарий, проходящий по всем разделам админки
+ * используем {@link LiteCartLoginTest} в качестве предэтапа
+ */
+public class Task7 extends LiteCartLoginTest {
+
+    @Test
+    public void sectionTest() {
+        String menuList = "//ul[@id='box-apps-menu']/li";
+        int menuSize = driver.findElements(By.xpath(menuList)).size();
+        int subMenuSize;
+        // прокликиваем все пункты меню
+        for (int i = 1; i <= menuSize; i++) {
+            // нажимаем на пункт меню
+            driver.findElement(By.xpath(menuList + "[" + i + "]")).click();
+            // проверяем наличие заголовка
+            Assert.assertTrue(isElementPresent(By.xpath("//h1")));
+            // проверяем наличие подменю, если есть -> прокликиваем
+            if ((subMenuSize = driver.findElements(
+                    By.xpath(menuList + "[" + i + "]//li")).size()) > 0) {
+                for (int j = 1; j <= subMenuSize; j++) {
+                    driver.findElement(By.xpath(menuList + "[" + i + "]//li[" + j + "]")).click();
+                    // проверяем наличие заголовка
+                    Assert.assertTrue(isElementPresent(By.xpath("//h1")));
+                }
+            }
+        }
+    }
+}
