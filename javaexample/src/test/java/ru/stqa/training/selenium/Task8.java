cdde2fc1cd3cd1a0efe163f9c3e01ab91eb3331f
abodykin <abodykin@inttrust.ru>
2017-10-15 14:05:53 +0300

    Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров

---
+++ javaexample/src/test/java/ru/stqa/training/selenium/Task8.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Anton on 15 Окт., 2017.
+ */
+package ru.stqa.training.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * лекция: 4. Поиск элементов
+ * задание: Задание 8. Сделайте сценарий, проверяющий наличие стикеров у товаров
+ * используем {@link LiteCartLoginTest} в качестве предэтапа
+ */
+public class Task8 extends BaseHelper {
+
+    @Test
+    public void stickerTest() {
+        driver.get("http://localhost/litecart/en/");
+        List<WebElement> goods = driver.findElements(
+                By.xpath("//*[@class='product column shadow hover-light']"));
+        // для каждого элемента-товара проверяем наличие одного! стикера
+        for(WebElement element : goods) {
+            Assert.assertTrue(element.findElements(
+                    By.xpath(".//div[contains(@class, 'sticker')]")).size() == 1);
+        }
+    }
+}
