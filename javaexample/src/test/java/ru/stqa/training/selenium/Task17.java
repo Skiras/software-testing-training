935765384c25e59f5c5eadf9f48ac8ad34e378eb
abodykin <abodykin@inttrust.ru>
2017-10-22 21:43:44 +0300

    Задание 17. Проверьте отсутствие сообщений в логе браузера

---
+++ javaexample/src/test/java/ru/stqa/training/selenium/Task17.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Anton on 22 Окт., 2017.
+ */
+package ru.stqa.training.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * лекция: 10. Протоколирование
+ * задание: Задание 17. Проверьте отсутствие сообщений в логе браузера
+ */
+public class Task17 extends LiteCartLoginTest {
+
+  @Test
+  public void protocolTest() {
+    driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+    List<WebElement> elements = driver.findElements(By.xpath("//*[@class='dataTable']//img/following-sibling::a"));
+    for (WebElement element : elements) {
+      switchTabDoSmthClose(element, null);
+      driver.manage().logs().get("browser").getAll().forEach(System.out::println);
+    }
+  }
+}
